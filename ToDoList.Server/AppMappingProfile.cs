7b1020130f880bc8bceba0d5b38b22335f11c794
Andrii Hretskyi <andreygretzky@gmail.com>
2023-05-09 15:46:35 +0300

    add graph ql

--- ToDoList.Server/AppMappingProfile.cs
+++ ToDoList.Server/AppMappingProfile.cs
@@ -1,8 +1,6 @@
 ﻿using AutoMapper;
-
-using ToDoList.ViewModels.Categories;
-using ToDoList.ViewModels.ToDos;
-using static ToDoList.ViewModels.ToDos.ToDosIndexViewModel;
+using ToDoList.GraphQL.Modules.Categories.DTO;
+using ToDoList.GraphQL.Modules.ToDos.DTO;
 
 namespace ToDoList
 {
@@ -10,19 +8,11 @@ namespace ToDoList
     {
         public AppMappingProfile()
         {
-            CreateMap<ToDoModel, ToDoIndexViewModel>().ReverseMap();
-            CreateMap<ToDoModel, ToDosDetailsViewModel>().ReverseMap();
-            CreateMap<ToDoModel, ToDosCreateViewModel>().ReverseMap();
-            CreateMap<ToDoModel, ToDoEditViewModel>().ReverseMap();
-            CreateMap<ToDoModel, ToDosDeleteViewModel>().ReverseMap();
+            CreateMap<ToDoModel, ToDosCreateInput>().ReverseMap();
+            CreateMap<ToDoModel, ToDosUpdateInput>().ReverseMap();
 
-            CreateMap<CategoryModel, CategoriesIndexViewModel>().ReverseMap();
-            CreateMap<CategoryModel, CategoriesDetailsViewModel>().ReverseMap();
-            CreateMap<CategoryModel, CategoriesCreateViewModel>().ReverseMap();
-            CreateMap<CategoryModel, CategoriesEditViewModel>().ReverseMap();
-            CreateMap<CategoryModel, CategoriesDeleteViewModel>().ReverseMap();
-
-         
+            CreateMap<CategoryModel, CategoriesCreateInput>().ReverseMap();
+            CreateMap<CategoryModel, CategoriesUpdateInput>().ReverseMap();
         }
     }
 }
--- ToDoList.Server/Program.cs
+++ ToDoList.Server/Program.cs
@@ -1,49 +1,50 @@
 using ToDoList;
-using ToDoList.Controllers;
-
+using ToDoList.GraphQL;
 using ToDoList.MsSql.Extensions;
-
 using ToDoList.XML.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddHttpContextAccessor();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DefaultPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+               .WithMethods("POST")
+               .WithOrigins("http://localhost:3000")
+               .AllowCredentials();
+    });
+});
 
 builder.Services.AddAutoMapper(typeof(AppMappingProfile));
 
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services.AddMsSqlDataProvider(builder.Configuration.GetConnectionString("MsSql"));
-}
-else
-{
-    builder.Services.AddMySqlDataProvider(Environment.GetEnvironmentVariable("JAWSDB_URL"));
-}
-
+builder.Services.AddMsSqlDataProvider(builder.Configuration.GetConnectionString("MsSql"));
 builder.Services.AddXmlDataProdiver(builder.Configuration.GetConnectionString("Xml"));
 
-
+builder.Services.AddGraphQLApi();
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("DefaultPolicy");
+}
+else
 {
     app.UseHsts();
 }
 
-app.UseStatusCodePagesWithReExecute("/error", "?statusCode={0}");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication();
+app.UseGraphQL<AppSchema>();
+app.UseGraphQLAltair();
 
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: $"{{controller={nameof(ToDosController).ReplaceInEnd("Controller", string.Empty)}}}/{{action={nameof(ToDosController.Index)}}}/{{id?}}");
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "wwwroot";
+});
 
 app.Run();
